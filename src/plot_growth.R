af8062d6fa64c39985bbd40fea276fda81db580f
Peyton Kuhlers <p.kuhl1711@gmail.com>
2022-03-02 07:09:24 -0500

    presentation update

--- src/plot_growth.R
+++ src/plot_growth.R
@@ -5,9 +5,10 @@ vol <- read.csv("source_data/pdx_colon.csv")
 theme_set(theme_classic())
 
 pdf("figures/growth_plots.pdf")
-ggplot(vol, aes(x = OBS_DAY, y = TUMOR_WT, color = ID)) +
-  geom_line(alpha = 0.5) +
+ggplot(vol, aes(x = OBS_DAY, y = log(TUMOR_WT))) +
+  geom_line(alpha = 0.5, aes(group = ID)) +
+  geom_smooth(method = 'lm', formula = y~x, se = F) +
   facet_wrap(~ AgentName) +
   guides(color = "none") +
-  labs(x = "Observation Day", y = expression(paste("Tumor Volume (m", m^3, ")")))
+  labs(x = "Observation Day", y = expression(paste("Log Tumor Volume (m", m^3, ")")))
 dev.off()
