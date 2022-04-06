6da003ba2589ef06e009577fa05a014e91018e51
Peyton Kuhlers <p.kuhl1711@gmail.com>
2022-04-06 14:53:22 -0400

    testing diff models

--- src/aft.R
+++ src/aft.R
@@ -3,15 +3,11 @@ library(tidyverse)
 library(survival)
 growth <- read.csv("source_data/pdx_colon.csv") %>%
   mutate(day = OBS_DAY - 7,
-         drug = relevel(factor(AgentName), ref = "Control"),
-         censor = 1)
+         drug = relevel(factor(AgentName), ref = "Control"))
 gg <- growth %>%
   group_by(ID, drug) %>%
   summarize(maxDay = max(OBS_DAY)) %>%
   mutate(censor = 1)
 aft <- survreg(Surv(maxDay, censor) ~ drug, data = gg)
-
-growth <- read.csv("source_data/pdx_pancreas.csv") %>%
-  mutate(day = OBS_DAY - 7,
-         drug = relevel(factor(AgentName), ref = "Control"),
-         censor = 1)
+summary(aft)
+saveRDS(aft, "derived_data/aft_model.rds")
--- src/mixed.R
+++ src/mixed.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lme4)
 growth <- read.csv("source_data/pdx_colon.csv") %>%
   mutate(day = OBS_DAY - 7,
+         month = OBS_DAY / 30,
          drug = relevel(factor(AgentName), ref = "Control"))
 
 ## OLS estimates
@@ -24,16 +25,23 @@ summary(fit1)
 fit2 <- glmer(
   TUMOR_WT ~ OBS_DAY + OBS_DAY:drug + (1 + OBS_DAY|ID),
   data = growth,
-  family = gaussian(link = "log"), 
+  family = gaussian(link = "log")
 )
 summary(fit2)
+
+## Random slope and intercept by mouse -- by month
+fitM <- glmer(
+  TUMOR_WT ~ month + month:drug + (1 + month|ID),
+  data = growth,
+  family = gaussian(link = "log")
+)
+saveRDS(fitM, "derived_data/colon_month_glmer.rds")
 
 ## Exploring some different model fitting methods
 library(nlme)
 library(MASS)
-fit <- lme(TUMOR_WT ~ OBS_DAY + OBS_DAY:drug, random = ~ OBS_DAY | ID, data = growth2)
-summary(fit)
-fit <-
+## log-linear model fit with PQL
+fit_day_pql <-
   MASS::glmmPQL(
     TUMOR_WT ~ OBS_DAY + OBS_DAY:drug,
     random = ~ OBS_DAY |
@@ -41,4 +49,15 @@ fit <-
     family = gaussian(link = "log"),
     data = growth
   )
-summary(fit)
+summary(fit_day_pql)
+
+fit_month_pql <-
+  MASS::glmmPQL(
+    TUMOR_WT ~ month + month:drug,
+    random = ~ month |
+      ID,
+    family = gaussian(link = "log"),
+    data = growth
+  )
+summary(fit_month_pql)
+saveRDS(fit, "derived_data/colon_kk")
