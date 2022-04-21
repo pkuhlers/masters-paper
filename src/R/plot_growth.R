7e1d627cb38bd88455b131e2879c7b8df95ce437
Peyton Kuhlers <p.kuhl1711@gmail.com>
2022-04-21 02:24:33 -0400

    plot pdf and small label changes

--- src/R/plot_growth.R
+++ src/R/plot_growth.R
@@ -23,30 +23,38 @@ ggplot(vol, aes(x = days_since_first, y = log(TUMOR_WT), color = ExpNameCSV)) +
   labs(x = "Observation Day", y = expression(paste("Log Tumor Volume (m", m^3, ")")))
 
 ## Readjusted time origin facet by drug
+pdf("figures/growth_rescaled_days_drug_facet.pdf")
 ggplot(vol, aes(x = days_since_first, y = log(TUMOR_WT))) +
   geom_line(alpha = 0.5, aes(group = ID)) +
   geom_smooth(method = 'lm', formula = y~x, se = F) +
   facet_wrap(~ AgentName) +
   guides(color = "none") +
   labs(x = "Observation Day", y = expression(paste("Log Tumor Volume (m", m^3, ")")))
+dev.off()
 
 ## Readjust time origin and slopes for each drug
+pdf("figures/growth_rescaled_days.pdf")
 ggplot(vol, aes(x = days_since_first, y = log(TUMOR_WT))) +
   geom_line(alpha = 0.2, aes(group = ID)) +
   geom_smooth(method = 'lm', formula = y~x, se = F, aes(color = AgentName)) +
-  labs(x = "Observation Day", y = expression(paste("Log Tumor Volume (m", m^3, ")")))
+  labs(x = "Days Since Staging", y = expression(paste("Log Tumor Volume (m", m^3, ")")))
+dev.off()
 
 ## Maximum volume distribution
+pdf("figures/max_vol_hist.pdf")
 ggplot(vol, aes(x = maxVol)) +
   geom_histogram(bins = 20, color = "black", fill = "lightblue") +
   geom_vline(xintercept = quantile(vol$maxVol, 0.25), linetype = "dashed") +
   labs(x = expression(paste("Tumor Volume (m", m^3, ")")),
-       y = "Frequency",
-       title = "Maximum Achieved Tumor Volume")
+       y = "Frequency")
+       # title = "Maximum Achieved Tumor Volume")
+dev.off()
 
 ## Distribution of number of follow ups
+pdf("figures/total_obs_hist.pdf")
 ggplot(vol, aes(x = total_obs)) +
   geom_histogram(bins = 15, color = "black", fill = "lightblue") +
   labs(x = "Number of Observations",
-       y = "Frequency",
-       title = "Observations per Mouse")
+       y = "Frequency")
+       # title = "Observations per Mouse")
+dev.off()
