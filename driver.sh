3a2c9d09362c4bc25f5c64928f6e1eb04d5c4d64
Peyton Kuhlers <p.kuhl1711@gmail.com>
2022-04-26 15:35:21 -0400

    driver update

--- driver.sh
+++ driver.sh
@@ -3,3 +3,8 @@
 Rscript src/R/clean.R
 
 Rscript src/R/plot_growth.R
+Rscript src/R/aft.R
+Rscript src/R/mixed.R
+
+Rscript -e "rmarkdown::render('presentation_Kuhlers.Rmd')"
+Rscript -e "rmarkdown::render('paper_Kuhlers.Rmd')"
